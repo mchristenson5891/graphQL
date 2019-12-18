0908c9cf4c9f8bdb4414040d677ec3bf74bed158
Michael Christenson <mchristenson5891@gmail.com>
2019-12-18 09:48:13 -0800

    add api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const applyUpdateResult = result => prevState => ({
+  hits: [...prevState.hits, ...result.hits],
+  page: result.page,
+});
+
+const applySetResult = result => prevState => ({
+  hits: result.hits,
+  page: result.page,
+});
+
+const getHackerNewsUrl = (value, page) =>
+  `https://hn.algolia.com/api/v1/search?query=${value}&page=${page}&hitsPerPage=100`;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hits: [],
+      page: null,
+    };
+  }
+
+  onInitialSearch = e => {
+    e.preventDefault();
+    const { value } = this.input;
+    if (value === '') {
+      return;
+    }
+    this.fetchStories(value, 0);
+  };
+
+  fetchStories = (value, page) =>
+    fetch(getHackerNewsUrl(value, page))
+      .then(response => response.json())
+      .then(result => this.onSetResult(result, page));
+  onSetResult = (result, page) =>
+    page === 0
+      ? this.setState(applySetResult(result))
+      : this.setState(applyUpdateResult(result));
+
+  render() {
+    return (
+      <div className='page'>
+        <div className='interactions'>
+          <form type='submit' onSubmit={this.onInitialSearch}>
+            <input type='text' ref={node => (this.input = node)} />
+            <button type='submit'>Search</button>
+          </form>
+        </div>
+        <List list={this.state.hits} />
+      </div>
+    );
+  }
 }
 
+const List = ({ list }) => (
+  <div className='list'>
+    {list.map(item => (
+      <div className='list-row' key={item.objectID}>
+        <a href={item.url}>{item.title}</a>
+      </div>
+    ))}
+  </div>
+);
 export default App;
